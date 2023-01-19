d22b3a5f2972e2cecdaf2233275bfb892eef43e6
Suraj Yadav <76870806+surajy1@users.noreply.github.com>
2023-01-19 10:11:37 +0530

    Create intro1.cpp

---
+++ OOP (Object Oriented Programming)/Static member/intro1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Player {
+public:
+	static int count;
+	Player() {count++;}
+	~Player() {count--;}
+};
+
+int Player::count = 0;
+
+int main()
+{
+	Player p1;
+	cout << Player::count << " "; // 1
+	{	
+		Player p2;
+		cout << Player::count << " "; // 2
+	} 
+	cout << Player::count << " "; // 1
+
+	return 0;
+}
