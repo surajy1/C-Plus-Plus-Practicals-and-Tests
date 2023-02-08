6d8ca21368c0e910027e2556b4528aa7c4a4c26c
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-02-08 15:20:14 +0530

    Create find_if.cpp

---
+++ Lambda Expressions/find_if.cpp
@@ -0,0 +1,17 @@
+// count_if
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	vector<int> v{10, 5, 3, 20, 100};
+
+	auto it = find_if(v.begin(), v.end(), [](int x) {return x < 10;});
+
+	cout << *it;
+
+	return 0;
+}
