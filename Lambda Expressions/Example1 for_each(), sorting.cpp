74f41bbdfb3952fafd16f2cf281e25dedd896e0f
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-02-08 15:14:35 +0530

    Create Example1 for_each(), sorting.cpp

---
+++ Lambda Expressions/Example1 for_each(), sorting.cpp
@@ -0,0 +1,18 @@
+// Lambda expressions
+// Sorting
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	vector<int> v{10, 20, 30};
+
+	for_each(v.begin(), v.end(), [](int &x) {x = x * 2;});
+
+	for_each(v.begin(), v.end(), [](int x) {cout << x << " ";});
+
+	return 0;
+}
