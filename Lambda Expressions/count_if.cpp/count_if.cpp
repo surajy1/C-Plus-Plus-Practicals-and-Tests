a8ab35c4609d1d8c06759ca95dc7e6d7b163393f
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-02-08 15:16:54 +0530

    Create count_if.cpp

---
+++ Lambda Expressions/count_if.cpp/count_if.cpp
@@ -0,0 +1,17 @@
+// count_if
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	vector<int> v{10, 5, 3, 20, 100};
+
+	int res = count_if(v.begin(), v.end(), [](int x) {return x >= 10;});
+
+	cout << res << " ";
+
+	return 0;
+}
