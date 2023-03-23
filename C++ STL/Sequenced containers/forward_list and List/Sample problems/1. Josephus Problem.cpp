7046a3f8728d920dc3d29ccd1c985d33078d48a6
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-03-23 17:13:11 +0530

    Add files via upload

---
+++ C++ STL/Sequenced containers/forward_list and List/Sample problems/1. Josephus Problem.cpp
@@ -0,0 +1,49 @@
+#include <list>
+#include <iostream>
+using namespace std;
+
+/*
+	1 -
+	Input: n = 7, k = 3
+	Output: 3
+
+	2 -
+	Input: n = 8, k = 2
+	Output: 0
+*/
+
+int getSurvivor(int n, int k) {
+	list<int> l;
+
+	for (int i = 0; i < n; i++)
+		l.push_back(i);
+
+	auto it = l.begin();
+
+	while (l.size() > 1)
+	{
+		for (int count = 1; count < k; count++)
+		{
+			it++;
+			if (it == l.end())
+				it = l.begin();
+		}
+
+		it = l.erase(it);
+		if (it == l.end())
+			it = l.begin();
+	}
+
+	return *l.begin();
+}
+
+int main() {
+
+	int n, k;
+
+	cin >> n >> k;
+
+	cout << getSurvivor(n, k);
+
+	return 0;
+}
