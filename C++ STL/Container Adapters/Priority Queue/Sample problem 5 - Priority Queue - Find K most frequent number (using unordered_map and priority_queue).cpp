e63b9704ed7c23573a145e636ec075b67c4c34cb
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-01 15:48:14 +0530

    Add files via upload

---
+++ C++ STL/Container Adapters/Priority Queue/Sample problem 5 - Priority Queue - Find K most frequent number (using unordered_map and priority_queue).cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+
+/*
+    Purchasing maximum items
+    Input 1: arr[] = {10, 5, 20, 5, 10, 10, 30}, k = 2
+    Output 1: 10 5
+
+    Input 2: arr[] = {20, 40, 30, 20, 30, 40, 60, 60}, k = 3
+    Output 2: 20 30 40
+*/
+
+struct MyCmp {
+    bool operator()(pair <int, int> p1, pair<int, int>p2)
+    {
+        if (p1.second == p2.second)
+            return p1.first > p2.first;
+        return p1.second < p2.second;
+    }
+};
+
+void printKMostFreq(int arr[], int n, int k)
+{
+    unordered_map<int, int> m;
+
+    for (int i = 0; i < n; i++)
+        m[arr[i]]++;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, MyCmp > pq(m.begin(), m.end());
+
+    for (int i = 0; i < k; i++)
+    {
+        cout << pq.top().first << " ";
+        pq.pop();
+    }
+}
+
+// TC: O(k log n)
+
+int main() {
+
+    int arr[] = {20, 40, 30, 20, 30, 40, 60, 60};
+    int k = 3;
+
+    printKMostFreq(arr, 8, k);
+
+    return 0;
+}
