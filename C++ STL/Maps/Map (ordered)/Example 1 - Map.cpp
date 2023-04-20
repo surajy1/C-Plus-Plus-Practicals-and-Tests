7ccca825866d2a17c424f046732001b1ccda994f
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-04-20 17:34:29 +0530

    Create Example 1 - Map.cpp

---
+++ C++ STL/Maps/Map (ordered)/Example 1 - Map.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+/*
+Input: 
+7
+1 2 3 1 3 2 12
+5
+1
+2
+3
+4
+12
+
+Output:
+1->2
+2->2
+3->2
+12->1
+2
+2
+2
+0
+1
+*/
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+
+    map<int, int> mpp;
+    // map<key_type, value_type>
+
+    // pre-compute
+    for (int i = 0; i < n; i++) {
+      mpp[a[i]]++;
+    }
+
+    // iterate in the map
+    for (auto it : mpp)
+      cout << it.first << "->" << it.second << endl;
+
+    // queries for frequencies
+    int q; cin >> q;
+    while (q--) {
+      int num;
+      cin >> num;
+      cout << mpp[num] << "\n";
+    }
+
+    return 0;
+}
